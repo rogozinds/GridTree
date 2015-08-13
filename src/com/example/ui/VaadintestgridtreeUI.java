2a94c4376743ff326f1bc754f525afb409027099
rogozinds <rogozinds@gmail.com>
2015-08-13 18:42:34 +0300

    Add hierarchy random generation

--- src/com/example/ui/VaadintestgridtreeUI.java
+++ src/com/example/ui/VaadintestgridtreeUI.java
@@ -4,9 +4,6 @@ import java.util.Random;
 
 import javax.servlet.annotation.WebServlet;
 
-import org.vaadin.gridtree.GridTree;
-import org.vaadin.gridtree.GridTreeContainer;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.Item;
@@ -21,6 +18,9 @@ import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+
+import org.vaadin.gridtree.GridTree;
+import org.vaadin.gridtree.GridTreeContainer;
 
 @SuppressWarnings("serial")
 @Theme("gridtree")
@@ -44,15 +44,15 @@ public class VaadintestgridtreeUI extends UI {
 		setContent(layout);
 		buttonsLayout.setHeight("50px");
 		buttonsLayout.setWidth("600px");
-		Label info= new Label();
+		final Label info= new Label();
 		info.setValue("Specify number of items");
 		textField=new TextField();
 		textField.setValue(""+DEFAULT_ITEMS);
-		
+
 		addButton=new Button("Generate",e->{
 			addItemsClick();
 		});
-		
+
 		initObjects(DEFAULT_ITEMS);
 		layout.addComponent(buttonsLayout);
 		buttonsLayout.addComponent(info);
@@ -60,30 +60,29 @@ public class VaadintestgridtreeUI extends UI {
 		buttonsLayout.addComponent(addButton);
 		grid.setSizeFull();
 		layout.addComponent(grid);
-		
+
 	}
 	private void addItemsClick() {
-		String value=textField.getValue();
+		final String value=textField.getValue();
 		int nItems=DEFAULT_ITEMS;
 		try {
 			nItems=Integer.parseInt(value);
-		} catch (NumberFormatException exc) {
+		} catch (final NumberFormatException exc) {
 			Notification.show("Wrong number", Notification.Type.ERROR_MESSAGE);
 		}
 		changeContainer(nItems);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void initObjects(int nItems) {
-		HierarchicalContainer container=createContainer(nItems);
-//		HierarchicalContainer container=oldContainer();
+		final HierarchicalContainer container=createContainer(nItems);
 		grid= new GridTree(new GridTreeContainer(container),"id");
 		grid.setColumnReorderingAllowed(true);
 		grid.getColumns().get(0).setWidth(200.5);
 	}
 	private void changeContainer(int nItems) {
-		HierarchicalContainer container=createContainer(nItems);
-		GridTreeContainer ct=new GridTreeContainer(container);
+		final HierarchicalContainer container=createContainer(nItems);
+		final GridTreeContainer ct=new GridTreeContainer(container);
 		grid.setContainerDataSource(ct);
 	}
 	private HierarchicalContainer createContainer(int nItems) {
@@ -94,23 +93,23 @@ public class VaadintestgridtreeUI extends UI {
 		final String[] lastName={"Black","White","Anaya","Anders","Andersen","Anderson","Andrade","Andre","Andres","Andrew","Andrews"};
 		final int  minIncome=1500;
 		final int maxIncome=4000;
-		final int nLevels=5;
-		HierarchicalContainer container=new HierarchicalContainer();
+
+		final HierarchicalContainer container=new HierarchicalContainer();
 		container.addContainerProperty("id", Integer.class, 0);
 		container.addContainerProperty("name", String.class, "");
 		container.addContainerProperty("lastName", String.class, "");
 		container.addContainerProperty("income", Integer.class, 0);
-		
+
 		for(int i=0;i<nItems;i++) {
-			Object itemId=""+i;
-			Item item=container.addItem(itemId);
+			final Object itemId=""+i;
+			final Item item=container.addItem(itemId);
 			item.getItemProperty("id").setValue(i);
 			item.getItemProperty("name").setValue(getValueFromArray(names));
 			item.getItemProperty("lastName").setValue(getValueFromArray(lastName));
 			item.getItemProperty("income").setValue(generateIncome(minIncome,maxIncome));
 			container.addItem(itemId);
 		}
-		createHierarcy(container);
+		createHierarcy(container,nItems);
 		Notification.show(nItems+ " created" );
 		return container;
 	}
@@ -121,29 +120,28 @@ public class VaadintestgridtreeUI extends UI {
 			}
 		}
 	}
-	private void createHierarcy(HierarchicalContainer container) {
-		addParent(container,"1", "0");
-		addParent(container,"2", "0");
-		addParent(container,"10", "1");
-		addParent(container,"11", "2");
-		addParent(container,"20", "10");
-		addParent(container,"21", "10");
-		addParent(container,"30", "11");
-		addParent(container,"15", "11");
-		addParent(container,"5", "20");
-		addParent(container,"5", "20");
-		addParent(container,"7", "20");
-		
+	private void createHierarcy(HierarchicalContainer container,int nItems) {
+		final int nLevels=5;
+		for(int i=0;i<nItems;i++) {
+			final String itemId=""+i;
+			if((i%nLevels)==0) {
+
+			}
+			else {
+				final String parent=(i-1)+"";
+				addParent(container,itemId,(i-1)+"");
+			}
+		}
 	}
 	private int generateIncome(int min,int max) {
-		Random rand=new Random();
-		int randomNum = rand.nextInt((max - min) + 1) + min;
+		final Random rand=new Random();
+		final int randomNum = rand.nextInt((max - min) + 1) + min;
 		return randomNum;
 	}
 	private String getValueFromArray(String[] list) {
-		int size=list.length;
-		Random rand=new Random();
-		int index=rand.nextInt(size);
+		final int size=list.length;
+		final Random rand=new Random();
+		final int index=rand.nextInt(size);
 		return list[index];
 	}
 }
